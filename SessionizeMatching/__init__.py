b82095e7be084ed523e2e8b08aad0b992be53e33
sb48 <sophiebiber@icloud.com>
2021-06-04 09:36:44 +0100

    Update __init__ to remove past pairings

--- SessionizeMatching/__init__.py
+++ SessionizeMatching/__init__.py
@@ -9,7 +9,6 @@ import json
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     matchRequest = req.params.get('matchRequest')
-    pastPairing = req.params.get('previousPairing')
     if not matchRequest:
         try:
             req_body = req.get_json()
@@ -19,8 +18,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             matchRequest = req_body.get('matchRequest')
 
     if matchRequest:
-        if not pastPairing:
-            result =  matching.match({}, matchRequest)
         result =  matching.match(pastPairing, matchRequest)
         return func.HttpResponse(body = json.dumps(result), mimetype = "application/json")
         # return func.HttpResponse(f"Hello, {matchRequest}. This HTTP triggered function executed successfully.")
